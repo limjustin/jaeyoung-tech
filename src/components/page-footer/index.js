3f9daf15a7a2b801a8b75af4c5cfb8a1c1f98492
Jaeyoung Lim <limjyjustin@gmail.com>
2024-12-31 14:06:15 +0900

    fix: 로고 수정 및 참조 추가

--- src/components/page-footer/index.js
+++ src/components/page-footer/index.js
@@ -6,7 +6,7 @@ function PageFooter({ author, githubUrl }) {
     <footer className="page-footer-wrapper">
       <p className="page-footer">
         © {new Date().getFullYear()}
-        &nbsp; &nbsp;powered by &nbsp;<a href={githubUrl}>{author}</a>
+        &nbsp; &nbsp;Thanks to &nbsp;<a href="https://www.linkedin.com/in/joy-shin/">Jooyoung Shin</a>
       </p>
     </footer>
   );
